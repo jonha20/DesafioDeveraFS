707b2fc5bc93ff81134bd0fe924f33c7645195dc
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-05-29 17:10:19 +0200

    server config ready

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express"); // Importamos el paquete express
+const app = express(); // Inciializar servidor con express
+const port = 3000; // Puerto a usar por el servidor
+const morgan = require('morgan');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+app.use(express.json());
+
+
+app.set('trust proxy', 1); // Habilitar el proxy para HTTPS
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:80",
+    "http://localhost"
+  ],
+  credentials: true
+}));
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../client')));
+
+
+// Handles any requests that don't match the ones above
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.get('/ready', (req, res) => {
+  res.send('Server Ready!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
