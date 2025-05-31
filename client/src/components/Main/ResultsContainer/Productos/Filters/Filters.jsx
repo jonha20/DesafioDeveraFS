121fa361a2ec76966ebdabf5f32be5afbdaff444
jonha20 <jonathan812000@gmail.com>
2025-06-01 01:55:51 +0200

    add/ paginacion, post de productos, estilos en filtros, show entries , lectura de json junto con bbdd, traducciones navvar y columnas en responsive

--- client/src/components/Main/ResultsContainer/Productos/Filters/Filters.jsx
+++ client/src/components/Main/ResultsContainer/Productos/Filters/Filters.jsx
@@ -1,20 +1,19 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { useDebounce } from "use-debounce";
 import { utils } from "xlsx";
 
-const Filters = ({setFilterProducto, data}) => {
+const Filters = ({ setFilterProducto, data, setPag }) => {
   const { t } = useTranslation();
   const [entries, setEntries] = useState(10);
   const csvLink = useRef(null);
   const [search, setSearch] = useState("");
   const [debouncedText] = useDebounce(search, 2000);
 
-
-
   useEffect(() => {
     setFilterProducto(debouncedText);
-  }, [debouncedText]);
+    setPag(entries); // Reset pagination when filter changes
+  }, [debouncedText , entries]);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -31,19 +30,31 @@ const Filters = ({setFilterProducto, data}) => {
     window.URL.revokeObjectURL(url);
   };
 
-
   return (
     <>
       <div className="filters">
-        {/* Aquí puedes quitar el select si no quieres */}
+       
         <div className="filters__left">
           <label className="filters__label">{t("Filter.show")}</label>
+           <select
+          name="entries"
+          id="entries"
+          value={entries}
+          onChange={(e) => setEntries(Number(e.target.value))}
+        >
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
           <span className="filters__label">{t("Filter.entries")}</span>
         </div>
         <div className="filters__center"></div>
         <div className="filters__right">
           <div className="filters__search">
-            <span className="filters__search-icon"><img src="/icons/search.svg" alt="search_svg" /></span>
+            <span className="filters__search-icon">
+              <img src="/icons/search.svg" alt="search_svg" />
+            </span>
             <input
               type="text"
               className="filters__input"
@@ -53,13 +64,26 @@ const Filters = ({setFilterProducto, data}) => {
             />
           </div>
 
-          <a href="#" className="filters__export" onClick={e => { e.preventDefault(); exportToCSV(); }}>
-            <span role="img" aria-label="export" className="filters__export-icon"><img src="/icons/file_save.svg" alt="search_svg" /></span>
+          <a
+            href="#"
+            className="filters__export"
+            onClick={(e) => {
+              e.preventDefault();
+              exportToCSV();
+            }}
+          >
+            <span
+              role="img"
+              aria-label="export"
+              className="filters__export-icon"
+            >
+              <img src="/icons/file_save.svg" alt="search_svg" />
+            </span>
             <div className="filters__export__text">{t("Filter.export")}</div>
           </a>
 
           {/* Enlace oculto para descarga */}
-          <a ref={csvLink} style={{display: "none"}}></a>
+          <a ref={csvLink} style={{ display: "none" }}></a>
         </div>
       </div>
     </>
