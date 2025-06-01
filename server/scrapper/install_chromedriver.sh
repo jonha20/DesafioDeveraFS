16016812c8b0bae4c9cc3e65b7aed43df29822f1
jonha20 <jonathan812000@gmail.com>
2025-06-01 14:17:03 +0200

    fix scrapper

--- server/scrapper/install_chromedriver.sh
+++ server/scrapper/install_chromedriver.sh
@@ -3,5 +3,6 @@ apt-get update
 apt-get install -y wget unzip
 wget -q https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-mv chromedriver /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
+mkdir -p bin
+mv chromedriver ./bin/chromedriver
+chmod +x ./bin/chromedriver
