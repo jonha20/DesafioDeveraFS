b002d73ee7b559f1a455f47af0758af4f81bf0a9
Michelle Alejandra Díaz Figueroa <midifi26@gmail.com>
2025-06-02 22:39:42 +0200

    NavDetailProduct hecho

--- client/src/components/Main/ResultsContainer/Productos/ProductDetail/NavDetailProduct/NavDetailProduct.jsx
+++ client/src/components/Main/ResultsContainer/Productos/ProductDetail/NavDetailProduct/NavDetailProduct.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
-const NavDetailProduct = () => {
-  return <div>NavDetailProduct</div>;
+const NavDetailProduct = ({ setActiveTab, activeTab }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <div className="navbar">
+        <h2
+          className={activeTab === "resumen" ? "active" : ""}
+          onClick={() => setActiveTab("resumen")}
+        >
+          {t("NavProductos.Resumen")}
+        </h2>
+        <h2
+          className={activeTab === "conclusions" ? "active" : ""}
+          onClick={() => setActiveTab("conclusions")}
+        >
+          {t("NavProductos.Conclusiones")}
+        </h2>
+        <h2
+          className={activeTab === "detailsCat" ? "active" : ""}
+          onClick={() => setActiveTab("detailsCat")}
+        >
+          {t("NavProductos.DetallesCategoria")}
+        </h2>
+        <h2
+          className={activeTab === "comparative" ? "active" : ""}
+          onClick={() => setActiveTab("comparative")}
+        >
+          {t("NavProductos.Comparativa")}
+        </h2>
+        <h2
+          className={activeTab === "sustainability" ? "active" : ""}
+          onClick={() => setActiveTab("sustainabilityt")}
+        >
+          {t("NavProductos.SustenibilidadDeLaMarca")}
+        </h2>
+        <h2
+          className={activeTab === "infoMarketing" ? "active" : ""}
+          onClick={() => setActiveTab("infoMarketing")}
+        >
+          {t("NavProductos.InformacionMarketing")}
+        </h2>
+      </div>
+      <div className="navbar-underline"></div>
+    </>
+)
 };
 
 export default NavDetailProduct;
