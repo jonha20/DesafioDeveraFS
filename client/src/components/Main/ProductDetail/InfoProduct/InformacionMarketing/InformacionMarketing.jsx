b08f2329cf50fabe474dd17239b68c9816bec2b7
mario-olopez <mariolopezclav@gmail.com>
2025-06-02 17:01:37 +0200

    fixed/structure and components of InfoProduct

---
+++ client/src/components/Main/ProductDetail/InfoProduct/InformacionMarketing/InformacionMarketing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InformacionMarketing = () => {
+  return <div>InformacionMarketing</div>;
+};
+
+export default InformacionMarketing;
---
+++ client/src/components/Main/ProductDetail/InfoProduct/Resumen/Resumen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Resumen = () => {
+  return <div>Resumen</div>;
+};
+
+export default Resumen;
