9f5ac98627a74ee9f90dfce88ace17a1f06d2d77
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-06-03 14:34:34 +0200

    add/analyze, form fix,

--- client/src/components/Main/ResultsContainer/Archivos/Archivos.jsx
+++ client/src/components/Main/ResultsContainer/Archivos/Archivos.jsx
@@ -9,20 +9,13 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
 
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  apiKey: "AIzaSyDG74mnKQtfL91ZMNeLSb-K0-mVzeAqWgs",
-  authDomain: "deveraai.firebaseapp.com",
-  projectId: "deveraai",
-  storageBucket: "deveraai.firebasestorage.app",
-  messagingSenderId: "950675463777",
-  appId: "1:950675463777:web:519c3130ac24c1cabe9420",
-  measurementId: "G-FXLWLGS5XE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
